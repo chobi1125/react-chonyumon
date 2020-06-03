5bd520873e5dfc5d43dfcf58b38d655092931971
chobi1125 <chonmage112000@yahoo.co.jp>
2020-06-03 13:45:01 +0900

    todoブランチにカウンターアプリ

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers } from 'redux';
 import { Provider } from 'react-redux';
-import App from './App-function';
+import App from './App';
 
 let state_value = {
   counter: 0,
